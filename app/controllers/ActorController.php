4ad795430419baebf2f6fde9879923434ec94f73
Tonitf <subcero00@hotmail.com>
2025-02-17 19:18:39 +0100

    actor

--- app/controllers/ActorController.php
+++ app/controllers/ActorController.php
@@ -1,8 +1,17 @@
 <?php
 require_once("./core/Controller.php");
-class ActorController extends Controller{
-public function index(...$params){//los tres puntos indican que se recibirá un número variable de parámetros
-    echo "Hola desde Index de ActorController";
+class ActorController extends Controller
+{
+    public function index(...$params)
+    { //los tres puntos indican que se recibirá un número variable de parámetros
+        echo "Hola desde Index de ActorController";
+    }
+    public function new(...$params)
+    {
+        if(isset($_POST["first_name"])){//Si se ha recibido un nombre
+            echo "Se ha recibido un nuevo actor";
+    }else{
+        $this->view("new_actor");
+    }
     }
 }
-?>
