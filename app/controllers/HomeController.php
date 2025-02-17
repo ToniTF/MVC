2ff9a615720471c22e43a341a3a685b821072c74
Tonitf <subcero00@hotmail.com>
2025-02-17 19:28:04 +0100

    vendor

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,12 +1,14 @@
 <?php
 require_once("./core/Controller.php");
-class HomeController extends Controller{
- public function index(...$params){
-     $data=["mensaje"=>"Bienvenido a la página principal"];
-     $this->view("home",$data);
- }
-public function new(){
-    echo "Hola desde NEW de HomeController";
+class HomeController extends Controller
+{
+    public function index(...$params)
+    {
+        $data = ["mensaje" => "Bienvenido a la página principal"];
+        $this->view("home", $data);
+    }
+    public function new()
+    {
+        echo "Hola desde NEW de HomeController";
+    }
 }
-}
-?>
--- app/views/new_actor.php
+++ app/views/new_actor.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h3>Nuevo actor</h3>
     <form action="" method="post">
-<input type="text" name="first_name" placeholder="Nombre">
-<input type="text" name="last_name" placeholder="Apellido">
-<input type="submit" value="Guardar">
-
+        <input type="text" name="first_name" placeholder="Nombre">
+        <input type="text" name="last_name" placeholder="Apellido">
+        <input type="submit" value="Guardar">
     </form>
-    
+
 </body>
+
 </html>
