18886b680a744b3c3e520fb5be1ff4ad56b55523
Tonitf <subcero00@hotmail.com>
2025-02-18 17:19:30 +0100

    new film

---
+++ app/views/new_film.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nueva Película</title>
+</head>
+
+<body>
+    <h3>Nuevo actor</h3>
+    <form action="" method="post">
+        <input type="text" name="Título" placeholder="Título">
+        <input type="submit" value="Guardar">
+    </form>
+
+</body>
+
+</html>
